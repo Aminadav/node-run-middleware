d6f7dd3ec507265d2173d9534175ba9c359a72d4
AGLaptop <aLaptop@bubbles.co.il>
2016-09-10 23:53:40 +0300

    Add example.js

---
+++ example.js
@@ -0,0 +1,12 @@
+var express=require('express')
+var app=express()
+
+require('./index.js')(app)
+app.get('/get-user/:id',function(req,res){
+	res.send({user:req.params.id,name:'Moyshale'})
+})
+
+app.runMiddleware('/get-user/20',{},function(code,data){
+	console.log(code) // 200 
+	console.log(data) // { user: '20', name: 'Moyshale' }
+})
